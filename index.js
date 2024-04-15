79b12c94566aaaa2f6ef73b8109639fd550db5be
Harrison Royds <harrisonroyds@yahoo.co.uk>
2024-04-15 12:06:53 +0100

    done

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 function createRangeArr(arrayOfRanges) {
-    // enter solution here
+  // enter solution here
+  const arr = [];
+  if (arrayOfRanges.length === 0) {
+    return arr;
+  } else if (arrayOfRanges[0] === arrayOfRanges[1]) {
+    return [arrayOfRanges[1]];
+  } else if (arrayOfRanges[0] === arrayOfRanges[1] + 1) {
+    return arrayOfRanges.flat();
+  } else {
+    return [
+      createRangeArr([arrayOfRanges[0], arrayOfRanges[1] - 1]),
+      arrayOfRanges[1],
+    ].flat();
+  }
 }
 
-module.exports = { createRangeArr }
+module.exports = { createRangeArr };
